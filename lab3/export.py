4b0cb027232f02197100ccdd4428113ae0f67cc5
aa35037123 <wesleyshi0321@gmail.com>
2024-05-09 16:51:19 +0800

    ptq version

--- lab3/export.py
+++ lab3/export.py
@@ -1,54 +1,24 @@
 import torch
-from torch._export import capture_pre_autograd_graph
-from torch.export import export, ExportedProgram
-from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
-from torch import nn
-import pickle
-import io
-import pickle
-from torch.export import dynamic_dim
-from executorch.exir import ExecutorchBackendConfig, ExecutorchProgramManager, EdgeProgramManager, to_edge
-from executorch.exir.passes import MemoryPlanningPass
-import executorch.exir as exir
-NUM_CLASSES = 10
+from torch.export import export
+from executorch.exir import to_edge
 
+# Start with a PyTorch model that adds two input tensors (matrices)
+class Add(torch.nn.Module):
+  def __init__(self):
+    super(Add, self).__init__()
 
-if __name__ == '__main__':
-    # contents = CPU_Unpickler(f).load()
-    model = mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
-    model.classifier[1] = nn.Linear(model.classifier[1].in_features, NUM_CLASSES)
-    weight_path = '/home/aa35037123/Wesley/edge_ai/lab3/mobilenetv2_0.963.pth'
-    # with torch.loading_context(map_location='cpu'):
-    # weights = CPU_Unpickler(weight_path).load()
-    # Load the state_dict to the model
-    model = torch.load(weight_path, map_location=torch.device('cpu'))
-    model.to('cpu')
-    model.eval()
-    # model.load_state_dict(torch.load('/home/aa35037123/Wesley/edge_ai/lab1/mobilenet.pt'), map_location=torch.device('cpu'))
-    # image size is 224, batch is 1
-    example_args = (torch.randn(1, 3, 224, 224),)
-    pre_autograd_aten_dialect = capture_pre_autograd_graph(model, example_args)
-    print("Pre-Autograd ATen Dialect Graph")
-    print(pre_autograd_aten_dialect)
-    aten_dialect: ExportedProgram = torch.export.export(pre_autograd_aten_dialect, example_args)
-    print("ATen Dialect Graph")
-    print(aten_dialect)
+  def forward(self, x: torch.Tensor, y: torch.Tensor):
+      return x + y
 
-    
-    edge_program: EdgeProgramManager = exir.to_edge(aten_dialect)
-    print("Edge Dialect Graph")
-    print(edge_program.exported_program())
+# 1. torch.export: Defines the program with the ATen operator set.
+aten_dialect = export(Add(), (torch.ones(1), torch.ones(1)))
 
+# 2. to_edge: Make optimizations for Edge devices
+edge_program = to_edge(aten_dialect)
 
+# 3. to_executorch: Convert the graph to an ExecuTorch program
+executorch_program = edge_program.to_executorch()
 
-    # # Optionally do delegation:
-    # # edge_program = edge_program.to_backend(CustomBackendPartitioner)
-    executorch_program: ExecutorchProgramManager = edge_program.to_executorch(
-        ExecutorchBackendConfig(
-            passes=[],  # User-defined passes
-        )
-    )
-
-    with open("mobilenet.pte", "wb") as file:
-        file.write(executorch_program.buffer)
-
+# 4. Save the compiled .pte program
+with open("add.pte", "wb") as file:
+    file.write(executorch_program.buffer)
